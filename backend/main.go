92db3dd024cc7c981cd0325e2700c439863b1555
Custord <126922564+CustordSolos@users.noreply.github.com>
2025-11-24 14:01:44 +0000

    feat(backend): implement POST based on the To-Do schema

--- backend/main.go
+++ backend/main.go
@@ -23,8 +23,8 @@ func ToDoListHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
 		handleGetList(w, r)
-	// case http.MethodPost:
-	// 	handleAddTodo(w, r)
+	case http.MethodPost:
+		handleAddTodo(w, r)
 	default:
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
@@ -34,4 +34,27 @@ func handleGetList(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	encoder := json.NewEncoder(w)
 	encoder.Encode(todos)
+}
+
+func handleAddTodo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	// Decode and validate per openAPI spec
+	var t Todo
+
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&t)
+	if err != nil {
+		http.Error(w, "Failed to decode JSON, bad request", http.StatusBadRequest)
+	}
+
+	if t.Title == "" || t.Description == "" {
+		http.Error(w, "Missing fields", http.StatusBadRequest)
+	}
+
+	// Store in-memory and return serialised struct
+	todos = append(todos, t)
+
+	encoder := json.NewEncoder(w)
+	encoder.Encode(t)
 }
